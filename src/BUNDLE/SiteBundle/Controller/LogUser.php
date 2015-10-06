96f0b219ab710cc1a3b2908e3cf794a5ef7dd5ec
acte-online <contact@acte-online.com>
2015-10-06 09:19:45 +0200

    Update LogUser.php

--- src/BUNDLE/SiteBundle/Controller/LogUser.php
+++ src/BUNDLE/SiteBundle/Controller/LogUser.php
@@ -10,52 +10,52 @@ class LogUserController extends Controller
 {
   public function pageAction()
   {
-    ...
+    	...
   
-        	// IP si internet partagé
-		if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-			$ip=$_SERVER['HTTP_CLIENT_IP'];
-		}
-		// IP derrière un proxy
-		elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-			$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-		}
-		// Sinon : IP normale
-		else {
-			$ip=$_SERVER['REMOTE_ADDR'];
-		}		
-		// La chaîne qui décrit le client HTML utilisé pour voir la page courante. 
-		$useragent=$_SERVER['HTTP_USER_AGENT'];
-		
-		// La chaîne de requête, si elle existe, qui est utilisée pour accéder à la page.
-		$querystring=$_SERVER['QUERY_STRING'];
-		
-		// L'adresse de la page (si elle existe) qui a conduit le client à la page courante.
-		$uri=$_SERVER['REQUEST_URI'];
-        
-        	// if pour ne pas enregistrer les du réseau local..
-		if ($ip!='192.168.1.254'){
-			//INSERTION DE L'IP DANS DB MYSQL
-			$loguser = new Loguser();
-		    $loguser->setIp($ip);
-		    $loguser->setDateinsert(new \DateTime("now"));
-		    $loguser->setPage('/Nom-de-la-page');
-		    $loguser->setUseragent($useragent);
-		    $loguser->setQuerystring($querystring);
-		    $loguser->setUri($uri);
-		
-		    $em = $this->getDoctrine()->getManager();
-		
-		    $em->persist($loguser);
-		    $em->flush();
-		}
-		
-		return $this->render('ACTESiteBundle:Page:page.html.twig', array(
-        	'ip' => $ip,
-        	'useragent' => $useragent,
-        	'querystring' => $querystring,
-        	));
+	// IP si internet partagé
+	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+		$ip=$_SERVER['HTTP_CLIENT_IP'];
+	}
+	// IP derrière un proxy
+	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+	// Sinon : IP normale
+	else {
+		$ip=$_SERVER['REMOTE_ADDR'];
+	}		
+	// La chaîne qui décrit le client HTML utilisé pour voir la page courante. 
+	$useragent=$_SERVER['HTTP_USER_AGENT'];
+	
+	// La chaîne de requête, si elle existe, qui est utilisée pour accéder à la page.
+	$querystring=$_SERVER['QUERY_STRING'];
+	
+	// L'adresse de la page (si elle existe) qui a conduit le client à la page courante.
+	$uri=$_SERVER['REQUEST_URI'];
+	
+	// if pour ne pas enregistrer les du réseau local..
+	if ($ip!='192.168.1.254'){
+		//INSERTION DE L'IP DANS DB MYSQL
+		$loguser = new Loguser();
+	    $loguser->setIp($ip);
+	    $loguser->setDateinsert(new \DateTime("now"));
+	    $loguser->setPage('/Nom-de-la-page');
+	    $loguser->setUseragent($useragent);
+	    $loguser->setQuerystring($querystring);
+	    $loguser->setUri($uri);
+	
+	    $em = $this->getDoctrine()->getManager();
+	
+	    $em->persist($loguser);
+	    $em->flush();
+	}
+	
+	return $this->render('ACTESiteBundle:Page:page.html.twig', array(
+	'ip' => $ip,
+	'useragent' => $useragent,
+	'querystring' => $querystring,
+	));
   
-    ...
+    	...
   }
 }
